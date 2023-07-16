be44c549c7ba201d8dff5fe7bc5acdfd8c142037
sleverbor <sleverbor@gmail.com>
2023-07-16 11:57:03 -0700

    add token auth

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
 from .models import *
 from .serializers import *
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser,IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
@@ -17,11 +17,14 @@ def index(request):
 class MenuItemsView(ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
     
 class SingleMenuItemView(RetrieveUpdateAPIView,DestroyAPIView):
     queryset=Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
     
 class BookingViewSet(ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
